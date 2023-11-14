3577ee2fde49186d7e91bd2c5cc205f303d7e84b
harsh-shende <harshshende28@gmail.com>
2023-11-14 15:27:46 +0530

    Implementing Simple Linear Regression

--- src/main/java/dataAnalysisAlgorithms/findingCorrelationMatrix.java
+++ src/main/java/dataAnalysisAlgorithms/findingCorrelationMatrix.java
@@ -5,13 +5,99 @@ import tech.tablesaw.api.Table;
 import tech.tablesaw.columns.Column;
 
 public class findingCorrelationMatrix {
+    public static double[][] getCorrelationMatrix(Table table) {
+        //Creating variables
+        int variRows=0;
+        Table structureOfTable=table.structure();
+        int totalRows=table.rowCount();
+
+        //Storing attributes
+        String[] attr=table.columnNames().toArray(new String[0]);
+        int contVariCount=structureOfTable.stringColumn(2).isEqualTo("INTEGER").size();
+        contVariCount+=structureOfTable.stringColumn(2).isEqualTo("DOUBLE").size();
+
+        //Storing continuous variable name
+        String[] contVariNames=new String[contVariCount];
+        for(int i=0,j=0;i<table.columnCount() && j<contVariCount;i++) {
+            if(Objects.equals(structureOfTable.column(2).getString(i),"INTEGER")) {
+                contVariNames[j]=structureOfTable.column(1).getString(i);
+                j+=1;
+            }
+            if(Objects.equals(structureOfTable.column(2).getString(i),"DOUBLE")) {
+                contVariNames[j]=structureOfTable.column(1).getString(i);
+                j+=1;
+            }
+        }
+
+        //Creating array to store stats
+        double[] mean=new double[contVariCount];
+        double[] vari=new double[contVariCount];
+        double[] stdDevi=new double[contVariCount];
+        double[] sumiOfSqurOfDeviFromMean=new double[contVariCount];
+        double[][] deviFromMean=new double[contVariCount][table.rowCount()];
+        double[][] corrMatr=new double[contVariCount][contVariCount];
+
+        //Calculating mean and deviation from mean
+        for(int i=0;i<contVariCount;i++) {
+            variRows=0;mean[i]=0;
+            for(int j=0;j<totalRows;j++) {
+                if(!table.column(contVariNames[i]).isMissing(j)) {
+                    Column<?> desiVari=table.column(contVariNames[i]);
+                    String desiType=desiVari.type().name();
+                    if(desiType.equals("INTEGER")) {
+                        mean[i]+=table.intColumn(contVariNames[i]).getInt(j);
+                    }
+                    if(desiType.equals("DOUBLE")) {
+                        mean[i]+=table.doubleColumn(contVariNames[i]).getDouble(j);
+                    }
+                    variRows+=1;
+                }
+            }
+            mean[i]/=variRows;
+            for(int j=0;j<table.rowCount();j++) {
+                double temp=0;
+                if(!table.column(contVariNames[i]).isMissing(j)) {
+                    Column<?> desiVari=table.column(contVariNames[i]);
+                    String desiType=desiVari.type().name();
+                    if(desiType.equals("INTEGER")) {
+                        temp=table.intColumn(contVariNames[i]).getInt(j);
+                    }
+                    if(desiType.equals("DOUBLE")) {
+                        temp=table.doubleColumn(contVariNames[i]).getDouble(j);
+                    }
+                    deviFromMean[i][j]=mean[i]-temp;
+                    vari[i]+=Math.pow(deviFromMean[i][j],2);
+                }
+            }
+            sumiOfSqurOfDeviFromMean[i]=vari[i];
+            vari[i]/=(contVariCount-1);
+            stdDevi[i]=Math.sqrt(vari[i]);
+        }
+
+        //Calculating cross product of deviation
+        for(int i=0;i<contVariCount;i++) {
+            for(int j=0;j<contVariCount;j++) {
+                for(int k=0;k<table.rowCount();k++) {
+                    corrMatr[i][j]+=deviFromMean[i][k]*deviFromMean[j][k];
+                }
+            }
+        }
+
+        //Calculating and displaying correlation matrix
+        for(int i=0;i<contVariCount;i++) {
+            for(int j=0;j<contVariCount;j++) {
+                corrMatr[i][j]/=Math.sqrt(sumiOfSqurOfDeviFromMean[i]*sumiOfSqurOfDeviFromMean[j]);
+            }
+        }
+        return corrMatr;
+    }
     public static void main(String args[]) {
         //Creating variables
         int variRows=0;
         long startTime01=System.currentTimeMillis();
 
         //Importing data
-        Table table=Table.read().csv("C:\\Users\\Asus\\OneDrive\\Documents\\Data Science\\Datasets\\PokemonStats.csv");
+        Table table=Table.read().csv("C:\\Users\\Asus\\OneDrive\\Documents\\Data Science\\Datasets\\sample_corr_01.csv");
         Table structureOfTable=table.structure();
         Table summaryOfTable=table.summary();
         int totalRows=table.rowCount();
---
+++ src/main/java/dataAnalysisAlgorithms/simpleLinearRegression.java
@@ -0,0 +1,120 @@
+package dataAnalysisAlgorithms;
+
+import java.util.Objects;
+import tech.tablesaw.api.Table;
+import tech.tablesaw.columns.Column;
+import static dataAnalysisAlgorithms.findingCorrelationMatrix.getCorrelationMatrix;
+
+public class simpleLinearRegression {
+    public static void main(String args[]) {
+        //Importing data
+        Table table=Table.read().csv("C:\\Users\\Asus\\OneDrive\\Documents\\Data Science\\Datasets\\PokemonStats.csv");
+        double[][] corrMatr=getCorrelationMatrix(table);
+        Table structureOfTable=table.structure();
+        int totalRows=table.rowCount();
+
+        //Displaying content and structure
+        /*System.out.println(table);
+        System.out.println();
+        System.out.println(structureOfTable);
+        System.out.println();
+        System.out.println(summaryOfTable);
+        System.out.println();*/
+
+        //Storing attributes
+        String[] attr=table.columnNames().toArray(new String[0]);
+        int contVariCount=structureOfTable.stringColumn(2).isEqualTo("INTEGER").size();
+        contVariCount+=structureOfTable.stringColumn(2).isEqualTo("DOUBLE").size();
+
+        //Storing continuous variable name
+        String[] contVariNames=new String[contVariCount];
+        for(int i=0,j=0;i<table.columnCount() && j<contVariCount;i++) {
+            if(Objects.equals(structureOfTable.column(2).getString(i),"INTEGER")) {
+                contVariNames[j]=structureOfTable.column(1).getString(i);
+                j+=1;
+            }
+            if(Objects.equals(structureOfTable.column(2).getString(i),"DOUBLE")) {
+                contVariNames[j]=structureOfTable.column(1).getString(i);
+                j+=1;
+            }
+        }
+
+        //Iterating through each and every variable
+        for(int i=0;i<contVariCount;i++) {
+            if(table.column(contVariNames[i]).countMissing()!=0) {
+                int posi=0;
+                double maxi=0;
+                for(int j=0;j<contVariCount;j++) {
+                    if(i!=j) {
+                        if(corrMatr[i][j]>maxi) {
+                            maxi=corrMatr[i][j];
+                            posi=j;
+                        }
+                    }
+                }
+                System.out.println("The variable "+contVariNames[i]+" has maximum correlation with "+contVariNames[posi]+" with correlation value "+maxi);
+
+                //Calculating sum required for calculating slope and intercept
+                double variRows=0,sumiOfX=0,sumiOfY=0,sumiOfXY=0,sumiOfXSqr=0;
+                for(int j=0;j<totalRows;j++) {
+                    if(!table.column(contVariNames[i]).isMissing(j) && !table.column(contVariNames[posi]).isMissing(j)) {
+                        variRows+=1;
+                        double xval=0,yval=0;
+                        Column<?> yCol=table.column(contVariNames[i]);
+                        Column<?> xCol=table.column(contVariNames[posi]);
+                        String yType=yCol.type().name();
+                        String xType=xCol.type().name();
+                        if(yType.equals("INTEGER")) {
+                            yval=table.intColumn(contVariNames[i]).getInt(j);
+                        } else {
+                            yval=table.doubleColumn(contVariNames[i]).getDouble(j);
+                        }
+                        if(xType.equals("INTEGER")) {
+                            xval=table.intColumn(contVariNames[posi]).getInt(j);
+                        } else {
+                            xval=table.doubleColumn(contVariNames[posi]).getDouble(j);
+                        }
+                        sumiOfX+=xval;
+                        sumiOfY+=yval;
+                        sumiOfXY+=xval*yval;
+                        sumiOfXSqr+=xval*xval;
+                    }
+                }
+
+                System.out.println("The sum of "+contVariNames[i]+" is "+sumiOfX);
+                System.out.println("The sum of "+contVariNames[posi]+" is "+sumiOfY);
+                System.out.println("The sum of "+contVariNames[posi]+"*"+contVariNames[i]+" is "+sumiOfXY);
+                System.out.println("The sum of "+contVariNames[posi]+"*"+contVariNames[posi]+" is "+sumiOfXSqr);
+
+                //Calculating slope and intercept
+                double slope=(variRows*sumiOfXY-sumiOfX*sumiOfY)/(variRows*sumiOfXSqr-sumiOfX*sumiOfX);
+                double intercept=(sumiOfY-slope*sumiOfX)/variRows;
+                System.out.println("The linear regression equation for "+contVariNames[i]+" and "+contVariNames[posi]+" is "+contVariNames[i]+"="+slope+"*"+contVariNames[posi]+"+"+intercept);
+
+                //Predicting missing values
+                for(int j=0;j<totalRows;j++) {
+                    if(table.column(contVariNames[i]).isMissing(j) && !table.column(contVariNames[posi]).isMissing(j)) {
+                        double xval=0,yval=0;
+                        Column<?> xCol=table.column(contVariNames[posi]);
+                        Column<?> yCol=table.column(contVariNames[i]);
+                        String xType=xCol.type().name();
+                        String yType=yCol.type().name();
+                        if(xType.equals("INTEGER")) {
+                            xval=table.intColumn(contVariNames[posi]).getInt(j);
+                        } else {
+                            xval=table.doubleColumn(contVariNames[posi]).getDouble(j);
+                        }
+                        yval=slope*xval+intercept;
+                        if(yType.equals("INTEGER")) {
+                            table.intColumn(contVariNames[i]).set(j,(int)yval);
+                        } else {
+                            table.doubleColumn(contVariNames[i]).set(j,yval);
+                        }
+                        System.out.println("The predicted value of "+contVariNames[i]+" for row "+j+" is "+yval);
+                    }
+                }
+            }
+        }
+        table.write().toFile("C:\\Users\\Asus\\OneDrive\\Desktop\\newFile.csv");
+    }
+}
